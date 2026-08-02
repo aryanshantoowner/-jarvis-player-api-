e4b942627dc2e8ef823a998e50f7bbb21e03a86e
aryanshantoowner <abirahmedsabbir1@gmail.com>
2026-08-02 17:34:13 +0600

    Update player.js

--- routes/player.js
+++ routes/player.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const router = express.Router();
 
+router.get("/players", (req, res) => {
+  res.json({
+    success: true,
+    message: "Player API Working 🚀",
+    data: []
+  });
+});
+
+module.exports = router;
